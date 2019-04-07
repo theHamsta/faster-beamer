2f52b93f9a34c8b5cb29762977d84a95179956c8
Stephan Seitz <stephan.seitz@fau.de>
2019-04-07 17:44:23 +0200

    Add crates for LaTex compilation and PDF manipulation

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ extern crate serde_json;
 #[macro_use]
 extern crate lazy_static;
 extern crate clap;
+#[macro_use]
+extern crate log;
+extern crate pretty_env_logger;
+extern crate tectonic;
+extern crate latexcompile;
+extern crate lopdf;
 
 mod parsing;
 mod process_file;
@@ -14,7 +20,9 @@ use clap::{App, Arg};
 use std::{process, thread, time};
 
 fn main() {
-    let matches = App::new("codgen")
+    pretty_env_logger::init();
+
+    let matches = App::new("faster-beamer")
         .version("1.0")
         .author("Stephan Seitz <stephan.seitz@fau.de>")
         .about("Incremental compiler for Beamer LaTeX presentations")
@@ -32,15 +40,13 @@ fn main() {
         )
         .arg(
             Arg::with_name("OUTPUT")
-                .help("Sets the output file to use")
-                .required(true)
+                .help("Sets the input file to use")
                 .index(2),
         )
         .get_matches();
 
     let is_server_mode = matches.is_present("server");
     let input_file = matches.value_of("INPUT").unwrap();
-    let output_file = matches.value_of("OUTPUT").unwrap();
 
     if is_server_mode {
         use hotwatch::{Event, Hotwatch};
@@ -50,24 +56,23 @@ fn main() {
         hotwatch
             .watch(input_file, move |event: Event| {
                 if let Event::Write(_) = event {
-                    println!("Input file has changed.");
-                    let input_file = matches.value_of("INPUT").unwrap();
-                    let output_file = matches.value_of("OUTPUT").unwrap();
-                    process_file::process_file(input_file, output_file);
+                    info!("Input file has changed.");
+                    let input_file = &matches.value_of("INPUT").unwrap();
+                    process_file::process_file(input_file, &matches.clone());
                 }
                 if let Event::Remove(_) = event {
-                    println!("Input file deleted!");
+                    info!("Input file deleted!");
                     process::exit(0);
                 }
             })
             .expect("Failed to watch file!");
-        println!("Server mode");
-        println!("Watching {}", input_file);
+        info!("Server mode");
+        info!("Watching {}", input_file);
 
         loop {
             thread::sleep(time::Duration::from_millis(100));
         }
     } else {
-        process_file::process_file(input_file, output_file);
+        process_file::process_file(input_file, &matches);
     }
 }
--- src/process_file.rs
+++ src/process_file.rs
@@ -5,11 +5,19 @@
 //
 use crate::parsing;
 use crate::tree_traversal;
-use std::fs;
+use crate::tree_traversal::TraversalOrder;
+use clap::ArgMatches;
+use latexcompile::{LatexCompiler, LatexError, LatexInput};
+use lopdf::Document;
+use std::collections::HashMap;
+use std::fs::write;
+use std::io::prelude::*;
+use std::io::BufReader;
 use std::path::Path;
+use tectonic;
 use tree_sitter::Node;
 
-pub fn process_file(input_file: &str, output_file: &str) {
+pub fn process_file(input_file: &str, _app: &ArgMatches) {
     if !Path::new(&input_file).exists() {
         eprintln!("Could not open {}", input_file);
         return;
@@ -18,60 +26,81 @@ pub fn process_file(input_file: &str, output_file: &str) {
     let parsed_file = parsing::ParsedFile::new(input_file.to_string());
     println!("{}", parsed_file.syntax_tree.root_node().to_sexp());
 
-    let root_node = parsed_file.syntax_tree.root_node();
-    let mut stack = vec![root_node];
+    //let latex = parsed_file.file_content;
+    //let pdf_data: Vec<u8> = tectonic::latex_to_pdf(latex).expect("processing failed");
+    //println!("Output PDF size is {} bytes", pdf_data.len());
 
-    while !stack.is_empty() {
-        let current_node = stack.pop().unwrap();
-        if current_node.kind() != "ERROR" {
-            println!(
-                "\n{}:\n\t {}",
-                current_node.kind(),
-                parsed_file.get_node_string(&current_node)
-            );
-        }
+    let mut dict = HashMap::new();
+    dict.insert("test".into(), "Minimal".into());
+    // provide the folder where the file for latex compiler are found
+    let input = LatexInput::from("/home/stephan/projects/LMEbeamer4_4");
+    // create a new clean compiler enviroment and the compiler wrapper
+    let compiler = LatexCompiler::new(dict).unwrap();
+    // run the underlying pdflatex or whatever
+    let result = compiler.run(&input_file, &input).unwrap();
 
-        for i in (0..current_node.named_child_count()).rev() {
-            stack.push(current_node.named_child(i).unwrap());
-        }
-    }
+    let document = Document::load_from(&result[..]).unwrap();
+    let pages = document.get_pages();
+    println!("{} pages", pages.iter().len());
+    // copy the file into the working directory
+    let output = ::std::env::current_dir().unwrap().join("out.pdf");
+    assert!(write(output, result).is_ok());
 
-    let node_types = vec!["text_env", "group"];
-    for t in node_types {
-        let comments = parsed_file.get_nodes_of_type(t.to_string());
-        println!("");
-        println!("Found {} {}s:", comments.len(), t);
-        for c in comments {
-            let text = &parsed_file.file_content[c.start_byte()..c.end_byte()];
-            println!("{}", text);
-        }
-    }
+    //let root_node = parsed_file.syntax_tree.root_node();
+    //let mut stack = vec![root_node];
 
-    let node_types = vec!["text_env"];
-    let mut frames = Vec::new();
-    for t in node_types {
-        let comments = parsed_file.get_nodes_of_type(t.to_string());
-        println!("");
-        println!("Found {} {}s:", comments.len(), t);
-        for c in comments {
-            let children = tree_traversal::get_children(
-                c,
-                &|n: Node| {
-                    n.kind() == "begin"
-                        && parsed_file
-                            .get_node_string(&n)
-                            .to_string()
-                            .contains("{frame}")
-                },
-                true,
-            );
-            if children.len() == 1 {
-                println!("");
-                println!("{}", parsed_file.get_node_string(&c));
-                frames.push(c);
-            }
-        }
-        println!("Found {} frames", frames.len());
-    }
-    fs::write(output_file, parsed_file.file_content).expect("Unable to write output file");
+    //while !stack.is_empty() {
+    //let current_node = stack.pop().unwrap();
+    //if current_node.kind() != "ERROR" {
+    //println!(
+    //"\n{}:\n\t {}",
+    //current_node.kind(),
+    //parsed_file.get_node_string(&current_node)
+    //);
+    //}
+
+    //for i in (0..current_node.named_child_count()).rev() {
+    //stack.push(current_node.named_child(i).unwrap());
+    //}
+    //}
+
+    //let node_types = vec!["text_env", "group"];
+    //for t in node_types {
+    //let comments = parsed_file.get_nodes_of_type(t.to_string());
+    //println!("");
+    //println!("Found {} {}s:", comments.len(), t);
+    //for c in comments {
+    //let text = &parsed_file.file_content[c.start_byte()..c.end_byte()];
+    //println!("{}", text);
+    //}
+    //}
+
+    //let node_types = vec!["text_env"];
+    //let mut frames = Vec::new();
+    //for t in node_types {
+    //let comments = parsed_file.get_nodes_of_type(t.to_string());
+    //println!("");
+    //println!("Found {} {}s:", comments.len(), t);
+    //for c in comments {
+    //let children = tree_traversal::get_children(
+    //c,
+    //&|n: Node| {
+    //n.kind() == "begin"
+    //&& parsed_file
+    //.get_node_string(&n)
+    //.to_string()
+    //.contains("{frame}")
+    //},
+    //true,
+    //TraversalOrder::DepthFirst,
+    //);
+    //if children.len() == 1 {
+    //println!("");
+    //println!("{}", parsed_file.get_node_string(&c));
+    //frames.push(c);
+    //}
+    //}
+    //println!("Found {} frames", frames.len());
+    //}
+    //fs::write(output_file, parsed_file.file_content).expect("Unable to write output file");
 }
