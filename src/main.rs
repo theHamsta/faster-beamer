2a8aaceeb55d207f9b26880c86a4a153a8d30eeb
Stephan Seitz <stephan.seitz@fau.de>
2019-10-13 03:28:31 +0200

    Set default log level to "info" again

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,19 @@ mod process_file;
 mod tree_traversal;
 
 use clap::{App, Arg};
+use std::env;
 use std::env::current_dir;
 use std::path::Path;
 use std::{thread, time};
 
 fn main() {
-    pretty_env_logger::init();
+    if env::var("RUST_LOG").is_err() {
+        let mut builder = pretty_env_logger::formatted_builder();
+        builder.parse_filters("info");
+        builder.init();
+    } else {
+        pretty_env_logger::init();
+    }
 
     let matches = App::new("faster-beamer")
         .version("0.1.3")
